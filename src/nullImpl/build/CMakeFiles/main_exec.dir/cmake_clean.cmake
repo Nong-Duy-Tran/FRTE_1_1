8151a1e5549ad365eb44510fa00c435aa90eb95c
Tran Duy Long <geekologitech@gmail.com>
2025-07-14 16:50:19 +0700

    first commit

---
+++ src/nullImpl/build/CMakeFiles/main_exec.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "../main_exec"
+  "../main_exec.pdb"
+  "CMakeFiles/main_exec.dir/main.cpp.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/main_exec.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
