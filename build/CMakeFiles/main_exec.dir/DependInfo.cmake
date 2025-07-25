7d1b1201d30c58100adf976d4d7c439c5c1fedb2
Tran Duy Long <geekologitech@gmail.com>
2025-07-25 18:07:01 +0700

    update build to match ArcFace

---
+++ build/CMakeFiles/main_exec.dir/DependInfo.cmake
@@ -0,0 +1,27 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/long.tran1/workenv/frvt/11/src/nullImpl/ArcFace.cpp" "/home/long.tran1/workenv/frvt/11/build/CMakeFiles/main_exec.dir/ArcFace.cpp.o"
+  "/home/long.tran1/workenv/frvt/11/src/nullImpl/RetinaFace.cpp" "/home/long.tran1/workenv/frvt/11/build/CMakeFiles/main_exec.dir/RetinaFace.cpp.o"
+  "/home/long.tran1/workenv/frvt/11/src/nullImpl/main.cpp" "/home/long.tran1/workenv/frvt/11/build/CMakeFiles/main_exec.dir/main.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/home/long.tran1/workenv/frvt/11/src/nullImpl/../include"
+  "/home/long.tran1/workenv/frvt/11/src/nullImpl/../../../common/src/include"
+  "/home/long.tran1/workenv/frvt/11/src/nullImpl"
+  "/home/long.tran1/workenv/frvt/11/src/nullImpl/../include/onnxruntime"
+  "/home/long.tran1/workenv/frvt/11/src/nullImpl/../include/opencv2"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
